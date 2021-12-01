1c535ecb7890a5e24b565964bcfb350a7b29b375
shruthi093 <jayasruthi.l@gmail.com>
2021-12-01 18:34:02 +0530

    Dec-01-2021 ( props and export imports)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-const ProductCatalog=()=>{
+export const IntroDetails=(props)=>{
+console.log(props.intro.cName)
+  return(
+      <>
+      <h1>{props.intro.cName}  Product Catalog</h1>
+      <h2>Product Details for home appliances</h2>
+      </>
+  )
+}
+export const ProductCatalog=(props)=>{
   return(
 <>
-<img src="https://images-eu.ssl-images-amazon.com/images/I/41U+Y11LRRL._AC_SX184_.jpg"></img>
-<a href="https://www.amazon.in/dp/B07FKNC3RD/ref=s9_acsd_al_bw_c2_x_0_t?pf_rd_m=A1K21FY43GMZF8&pf_rd_s=merchandised-search-6&pf_rd_r=2TF4E9CXA55M0MM6KMHQ&pf_rd_t=101&pf_rd_p=6bcc725a-6681-4d25-bbbc-abe358743e98&pf_rd_i=15390366031">Buy here</a>
-<h3>Price : 34,000</h3>
+<img src={props.detail.imgSource}></img>
+<a href={props.detail.prodLink}>Buy here</a>
+<h3>Price : {props.detail.price}</h3>
 </>
   )
 }
-export default ProductCatalog
+
+//export default
+//export by component name
